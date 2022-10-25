ac5cf4eff5553483830fe451176684f2695a1486
sherif14 <85257402+sherif14@users.noreply.github.com>
2022-10-25 03:54:28 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,9 +21,9 @@
 
  /* Define Global Variables*/
  var i = 0;
+ var k=0;
  const sections = document.querySelectorAll('section');
  const ul = document.createElement("ul");
-//  const list = document.createElement("li");
  const lists = document.getElementsByTagName("li");
  
 
@@ -32,86 +32,98 @@
 
  /* End Global Variables*/
  /* Start Helper Functions*/
+
+
+ // build the nav
  for (section of sections){
      var lii = document.createElement("li");
      lii.innerHTML=`<a href="#${section.id}" class="menu__link" id="element${++i}">${section.id}</a> `;
      document.getElementById('navbar__list').appendChild(lii);
+     scrollToSection(lii, section);
      
     }
+
+    // Scroll to anchor ID using scrollTO event
+    function scrollToSection(lii, section){
+        lii.addEventListener('click', function(e){
+            e.preventDefault();
+            window.scrollTo({
+                top: section.offsetTop,
+                behavior:"smooth"
+            });
+        });
+    }
+
     
-    
-    
-    
 
-
-    //   function scrollBehavior(li, section){
-    //       li.addEventListener('click', function(){
-
-    //           window.scrollTo({
-    //               top: section.offsetTop,
-    //               behavior:"smooth"
-    //           });
-    //       });
-    //   }
-
-    //    list.forEach(element => {
-    //      element.click(function() {
-    //          $('html,body').animate({
-    //              scrollTop: $(section.id).offset().top},
-    //              'slow');
-    //      });
-          
-        //  var elmntToView = document.getElementById("sectionId");
-        //  elmntToView.scrollIntoView(); 
-
-        //  function scrollToSection(){
-            
-        //         list .forEach(element => {
-        //             element.addEventListener("click", function() {
-        //                 window.scrollTo({
-        //                   top:$("#section").offsetTop,
-        //                   behavior:"smooth"
-        //               });
-                     
-        //          });
-
-
-        //      })
-        //  }
-        //  scrollToSection();
-        for(list of lists){
-            
-            // list.addEventListener("click", function() {
-            //              window.scrollTo({
-            //                top:.offsetTop,
-            //                behavior:"smooth"
-            //            });
-               
-            //       });
-        }
-
-
- 
-    // function joinLink() {
-                  
-    //     // Create anchor element.
-    //     var a = document.createElement('a'); 
-          
-    //     // Create the text node for anchor element.
-    //     var link = document.createTextNode("This is link");
-          
-    //     // Append the text node to anchor element.
-    //     a.appendChild(link); 
-          
-    //     // Set the title.
-    //     a.title = "This is Link"; 
-          
-    //     // Set the href property.
-    //     a.href = "https://www.geeksforgeeks.org"; 
-          
-    //     // Append the anchor element to the body.
-    //     document.body.appendChild(a); 
+    // function activeSection (){
+    //     // Select all anchor using "menu__link" class
+    //     const navbarButtons = document.querySelectorAll(".menu__link")
+    //     sections.forEach((section, i)=>{
+    //         //Get the boundingrect for each section 
+    //         const sectionBond = section.getBoundingClientRect();
+    //         //Check if the section is in viewport or not 
+    //         if (sectionBond.top <= 380 && sectionBond.bottom >= 350){
+    //             //section in viewport accourding to top and bottom boundings
+    //             //add 'your-active-class' class to the specific section
+    //             section.classList.add("your-active-class");
+    //             //add 'active_button' class to the specific nav button according to section ID
+    //             navActive[i].classList.add("active_button");
+    //         } else{
+    //             //Remove both section and navButton active classes when section is off sight
+    //             section.classList.remove("your-active-class");
+    //             navActive[i].classList.remove("active_button");
+    //         }
+    //     })
     // }
+    document.addEventListener("scroll",function(){
+       
+        const navbarButtons = document.querySelectorAll(".menu__link")
+        navbarButtons.forEach((navbarButton)=>{
+            
+            const bounding = section.getBoundingClientRect();
+         
+            if (bounding.top <= 300 && bounding.bottom >= 300){
+               
+                navbarButton.classList.add("activated_button");
+            } else{
+                
+                
+                navbarButton.classList.remove("activated_button");
+            }
+        })
+    });
+    document.addEventListener("scroll",function(){
+        // Select all anchor using "menu__link" class
+        
+        sections.forEach((section)=>{
+            
+            const sectionInformation = section.getBoundingClientRect();
+             
+            if (sectionInformation.top <= 300 && sectionInformation.bottom >= 300){
+               
+                section.classList.add("your-active-class");
+               
+                
+            } else{
+                
+                section.classList.remove("your-active-class");
+               
+            }
+        })
+    });
+    
+    
+
+    
+    
+   
+    
+    
+    
+
+
+   
 
  
 
@@ -124,6 +136,7 @@
  */
 
 // build the nav
+
 // sections.forEach(createLink);
 
 
